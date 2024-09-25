883a1f3f6c42c7bcb30a3ca071b3a624d0bd9c6e
Kalmovic <joseph.ext@rallyrd.com>
2024-09-25 19:11:58 -0300

    feat: transfer feature

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import * as ReactDOM from "react-dom/client";
 import Dashboard from "./routes/dashboard.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Transfer from "./routes/transfer.tsx";
+import Transfer, { transferAction } from "./routes/transfer.tsx";
 import { Root } from "./routes/root.tsx";
 import { Web3Provider } from "./providers/wallet-provider.tsx";
 import { Login } from "./routes/login.tsx";
 import { ErrorBoundary } from "./components/error-boundary.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,7 @@ const router = createBrowserRouter([
           {
             path: "transfer",
             element: <Transfer />,
+            action: transferAction,
             errorElement: <ErrorBoundary />,
           },
         ],
@@ -39,5 +41,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Web3Provider>
     <RouterProvider router={router} />
+    <Toaster />
   </Web3Provider>
 );
--- src/providers/wallet-provider.tsx
+++ src/providers/wallet-provider.tsx
@@ -9,13 +9,13 @@ declare module "wagmi" {
   }
 }
 
-const config = createConfig(
+export const config = createConfig(
   getDefaultConfig({
     // Your dApps chains
     chains: [sepolia],
     transports: {
       // RPC URL for each chain
-      [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/demo"),
+      [sepolia.id]: http("https://rpc.sepolia.org"),
     },
 
     // Required API Keys
@@ -26,8 +26,6 @@ const config = createConfig(
 
     // Optional App Info
     appDescription: "Bitso Challenge App",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   })
 );
 
