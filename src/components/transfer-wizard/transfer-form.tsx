6cc9417d7c9a399b81c9a1fa933a1c93d1adab27
Kalmovic <joseph.ext@rallyrd.com>
2024-10-03 19:05:54 -0300

    improvement: add amount validation

--- src/components/transfer-wizard/transfer-form.tsx
+++ src/components/transfer-wizard/transfer-form.tsx
@@ -1,8 +1,12 @@
+import { useMemo } from "react";
 import * as Yup from "yup";
 import { ethers } from "ethers";
 import { Address } from "viem";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { AnimatePresence, motion } from "framer-motion";
+import { useAccountBalance } from "@/hooks/useAccountBalance";
+import { useTokenContext } from "@/context/token-context";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
@@ -14,29 +18,47 @@ type TransferFormProps = {
   onNext: (data: { recipient: string; amount: string }) => void;
 };
 
-const schema = Yup.object().shape({
-  recipient: Yup.string()
-    .required("Recipient address is required")
-    .test("is-valid-address", "Invalid Ethereum address", (value) =>
-      ethers.isAddress(value || "")
-    ),
-  amount: Yup.string()
-    .required("Amount is required")
-    .test(
-      "is-positive",
-      "Amount must be a positive number",
-      (value) => !isNaN(Number(value)) && parseFloat(value || "0") > 0
-    ),
-});
-
 export function TransferForm({ data, onClose, onNext }: TransferFormProps) {
+  const { formattedBalance } = useAccountBalance();
+  const { tokenSymbol } = useTokenContext();
+
+  const schema = useMemo(() => {
+    return Yup.object().shape({
+      recipient: Yup.string()
+        .required("Recipient address is required")
+        .test("is-valid-address", "Invalid Ethereum address", (value) =>
+          ethers.isAddress(value || "")
+        ),
+      amount: Yup.string()
+        .required("Amount is required")
+        .test(
+          "is-positive",
+          "Amount must be a positive number",
+          (value) => !isNaN(Number(value)) && parseFloat(value || "0") > 0
+        )
+        .test(
+          "is-less-than-balance",
+          "Amount exceeds available balance",
+          function (value) {
+            if (!formattedBalance) {
+              return true; // Alternatively, return false to enforce validation
+            }
+            return parseFloat(value || "0") <= parseFloat(formattedBalance);
+          }
+        ),
+    });
+  }, [formattedBalance]);
+
   const {
     handleSubmit,
     formState: { errors },
+    setError,
     register,
   } = useForm<Yup.InferType<typeof schema>>({
-    resolver: yupResolver(schema),
-    reValidateMode: "onSubmit",
+    resolver: yupResolver(schema, {
+      context: { availableBalance: formattedBalance },
+    }),
+    reValidateMode: "onChange",
     shouldFocusError: true,
     defaultValues: {
       amount: data.amount || "",
@@ -45,6 +67,17 @@ export function TransferForm({ data, onClose, onNext }: TransferFormProps) {
   });
 
   const onSubmit = (data: { recipient: string; amount: string }) => {
+    // just in case the validation on amount fails to check if theres enough balance
+    if (
+      parseFloat(data.amount) > parseFloat(formattedBalance) ||
+      !formattedBalance
+    ) {
+      setError("amount", {
+        type: "is-less-than-balance",
+        message: "Amount exceeds available balance",
+      });
+      return;
+    }
     onNext(data);
   };
 
@@ -65,11 +98,18 @@ export function TransferForm({ data, onClose, onNext }: TransferFormProps) {
               {...register("recipient")}
               className="bg-primary-foreground"
             />
-            {errors.recipient && (
-              <span className="text-destructive text-xs">
-                {errors.recipient.message}
-              </span>
-            )}
+            <AnimatePresence mode="popLayout">
+              {errors.recipient && (
+                <motion.span
+                  className="text-destructive text-xs"
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                >
+                  {errors.recipient.message}
+                </motion.span>
+              )}
+            </AnimatePresence>
           </div>
           <div className="grid gap-1.5">
             <Label htmlFor="amount">Amount</Label>
@@ -79,18 +119,28 @@ export function TransferForm({ data, onClose, onNext }: TransferFormProps) {
               {...register("amount")}
               className="bg-primary-foreground"
             />
-            {errors.amount && (
-              <span className="text-destructive text-xs">
-                {errors.amount.message}
-              </span>
-            )}
+            <span className="text-xs text-slate-500">
+              You have: {formattedBalance} {tokenSymbol} available
+            </span>
+            <AnimatePresence mode="popLayout">
+              {errors.amount && (
+                <motion.span
+                  className="text-destructive text-xs"
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                >
+                  {errors.amount.message}
+                </motion.span>
+              )}
+            </AnimatePresence>
           </div>
-          <div className="flex justify-end space-x-2">
+          <motion.div className="flex justify-end space-x-2" layout>
             <Button variant="outline" onClick={onClose} type="button">
               Cancel
             </Button>
             <Button type="submit">Next</Button>
-          </div>
+          </motion.div>
         </form>
       </CardContent>
     </>
